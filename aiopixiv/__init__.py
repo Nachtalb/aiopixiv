1e171dc2bb260888026ef6f40fa8f2a5c1cf2e91
Nachtalb <na@nachtalb.io>
2023-07-07 18:27:28 +0200

    Add easy imports

--- aiopixiv/__init__.py
+++ aiopixiv/__init__.py
@@ -1,7 +1,17 @@
-#  from .aapi import AppPixivAPI
-#  from .bapi import ByPassSniApi
-#  from .models._pixivobject import PixivObject
+# ruff: noqa: F401
+from ._api import PixivAPI
+from .models.authentication import AuthenticatedUser, AuthenticatedUserImageUrls, Authentication
+from .models.bookmark import BookmarkDetail, BookmarkDetailTag
+from .models.comment import Comment, Comments
+from .models.illust import Illust, Illusts, UserPreview
+from .models.manga import MangaIllust
+from .models.novel import Novel, NovelRecommended, Novels, NovelSeries, NovelTag
+from .models.page import Page
+from .models.privacy import PrivacyPolicy
+from .models.search import SearchIllustResult, SearchNovelResult, SearchUserResult, TrendingTag
+from .models.tag import Tag
+from .models.ugoira import UgoiraFrame, UgoiraIllust, UgoiraMetadata, UgoiraZipUrls
+from .models.urls import ImageUrls, MetaPagesUrls, MetaSinglePageUrl
+from .models.user import Profile, ProfileImageUrls, ProfilePublicity, User, UserDetail, Users, Workspace
 
-#  __all__ = ("AppPixivAPI", "ByPassSniApi", "PixivError")
-
-__version__ = "0.1.0"
+__version__ = "0.1.0a0"
